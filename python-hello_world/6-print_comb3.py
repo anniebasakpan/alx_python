eb8c2961c1274e67aa596bbd8affa7f914bc4487
anniebasakpan <anniebasakpan@gmail.com>
2023-12-02 10:53:17 +0100

    updated changes

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,3 +1,3 @@
 for i in range(10):
     for j in range(i+1, 10):
-        print("{:02d}".format(i) + "{:02d}".format(j), end=", " if i < 9 or j < 8 else "\n")
+        print("{:01d}{:01d}".format(i, j), end=", " if i < 8 or (i == 8 and j < 9) else "\n")
