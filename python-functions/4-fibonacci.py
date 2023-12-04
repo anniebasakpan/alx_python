05ff73affe1cd16d6261c6e1d9aea109acb65377
anniebasakpan <anniebasakpan@gmail.com>
2023-12-04 17:49:25 +0100

    printing Fibonacci list

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,3 +1,8 @@
 def fibonacci_sequence(n):
-    my_numbers_list = list(range(n))
-    return(my_numbers_list)
+    fibonacci_list = [0, 1] #initializing the sequence
+    while len(fibonacci_list) < n:
+        next_number = fibonacci_list[-1] + fibonacci_list[-2]
+        fibonacci_list.append(next_number)
+
+    return(fibonacci_list[:n])
+
