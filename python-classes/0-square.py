4d9656fed8d14b5f807f084f8ab16232adb6eebb
anniebasakpan <anniebasakpan@gmail.com>
2023-12-24 19:42:59 +0100

    Python classes

---
+++ python-classes/0-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 0-square.py
+# annie
+"""Define a Square Class"""
+class Square():
+    """Define a private attribute size"""
+    def __init__(self, size):
+        self.__size = size
---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+"""Define a Square Class"""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
