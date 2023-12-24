9bacee163c59501404db339364d1197986c1a79f
anniebasakpan <anniebasakpan@gmail.com>
2023-12-24 20:09:12 +0100

    tests

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -4,10 +4,12 @@
 class Square:
     """Represent a square."""
 
-    def __init__(self, size):
-        """Initialize a new Square.
-
-        Args:
-            size (int): The size of the new square.
-        """
+    def __init__(self, size = 0):
+        self.__size = 0
+    # Check if size is an integer
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+    # Check if size is less than 0
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
