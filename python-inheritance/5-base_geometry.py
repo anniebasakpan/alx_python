69b9ba073eca0b2f80f0448c0715eb71aedeff99
anniebasakpan <anniebasakpan@gmail.com>
2024-01-02 20:48:10 +0100

    tests

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,37 +1,38 @@
 """
-This module contains an empty class
+A module that have an empty class with override to dir() method
 """
 
-class meta_class:
-    """This is the meta class"""
-    
-    def __dir__(cls):
-        return [attribute for  attribute in super().__dir__() if attribute != "__init_subclass__"]
 
-class BaseGeometry(meta_class):
-    
-    """This is a geometry class"""
+class MetaClass(type):
+    """
+    Override dir() method to execlude __init__subclass__
+    """
     def __dir__(cls):
-        """
-        Method to remove the __init_subclass__
-        """
-        return [attribute for  attribute in super().__dir__() if attribute != "__init_subclass__"]
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+
+
+class BaseGeometry(metaclass=MetaClass):
+    """
+    BaseGeometry class that uses the overriden dir() method 
+    """
+    
+    def __dir__(cls):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
     
     def area(self):
+        """
+        A public method that raise an exception
+        """
         raise Exception ("area() is not implemented")
-    
     def integer_validator(self, name, value):
-        self.name = "name"
-         #if type(value) != int:
-            #raise TypeError ("{} must be an integer".format(name))
-        #if value <= 0:
-            #raise ValueError ("{} must be greater than 0".format(name))
-        if type(value) is int:
-            if value <= 0:
-                raise ValueError("{} must be greater than 0".format(name))
-            else:
-                self.value = value
-        else:
+        """
+        A public method that validates value:
+        - If value is not an integer: raise a TypeError exception, 
+          with the message <name> must be an integer
+        - If value is less or equal to 0: raise a ValueError exception with the 
+         message <name> must be greater than 0
+        """
+        if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
-        
-    
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
