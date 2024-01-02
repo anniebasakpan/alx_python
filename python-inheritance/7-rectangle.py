7cf3abef3977c29595f4d3a27f742d7da0da5ee1
anniebasakpan <anniebasakpan@gmail.com>
2024-01-02 20:39:53 +0100

    testing

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -12,12 +12,12 @@ class Rectangle (BaseGeometry):
     def __init__(self, width, height):
         #self.integer_validator("width", width)
         #self.integer_validator("height", height)
-        super().integer_validator("width",width)
-        super().integer_validator("height",height)
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
         self.__width = width
         self.__height = height
 
     def area(self):
-        return self.__width*self.__height
+        return self.__width * self.__height
     def __str__(self):
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
